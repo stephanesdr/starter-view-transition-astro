fc30b53f7b114ea0d2b9db8a7497b903c3ca31b1
David Vantaelen <vant.david@outlook.com>
2024-03-15 14:46:23 +0100

    Remove example

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,41 @@
-import { defineCollection, z } from 'astro:content';
+import { z, reference, defineCollection } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
+const articleCollection = defineCollection({
+    type: 'content', 
+    schema: z.object({
+        title: z.string(),
+        canonicalURL: z.string().url(),
+        image: z.string().optional(),
+        tags: z.array(reference('tags')),
 		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+		updatedDate: z.coerce.date().optional()
+    }),
 });
 
-export const collections = { blog };
+const tagCollection = defineCollection({
+    type: 'data', 
+    schema: z.object({
+        title: z.string(),
+        canonicalURL: z.string().url(),
+        image: z.string().optional(),
+        articles: z.array(reference('articles')),
+    }),
+});
+
+const authorCollection = defineCollection({
+    type: 'data', 
+    schema: z.object({
+        username: z.string(),
+        lastname: z.string(),
+        firstname: z.string(),
+        canonicalURL: z.string().url(),
+        blogs: z.array(z.string()),
+        image: z.string().optional(),
+    }),
+});
+
+export const collections = {
+    'articles': articleCollection,
+    'tags': tagCollection,
+    'authors': authorCollection,
+};
